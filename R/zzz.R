dfaabdf4420db45e6bc25667243cc7b097ead8c5
Rolf Simoes <rolfsimoes@gmail.com>
2018-08-08 22:28:19 -0300

    Update citations

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,11 @@
+# On load
+.onAttach = function(lib, pkg){
+    packageStartupMessage(
+        sprintf("Loaded inSitu e-sensing data sets. V%s. Use citation(\"inSitu\") for use in publication.",
+                utils::packageDescription("inSitu")$Version)
+    )
+}
+
 #' @import tibble
 #'
 NULL
