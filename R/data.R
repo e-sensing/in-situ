b93a282cdf1f6ad01c87f352958e41cea612e40e
Rolf Simoes <rolfsimoes@gmail.com>
2018-10-17 15:18:13 -0300

    New data set "br_mt_1_8K_9classes_6bands"

--- R/data.R
+++ R/data.R
@@ -13,7 +13,8 @@
 list_datasets <- function() {
   return(c("cerrado_124K_16classes_6bands",
            "cerrado_64K_13classes_6bands",
-           "br_mt_2K_9classes_6bands"))
+           "br_mt_2K_9classes_6bands",
+           "br_mt_1_8K_9classes_6bands"))
 }
 
 #' @title Samples of Cerrado biome
@@ -88,7 +89,7 @@ NULL
 #'   coverage (the name of the coverage associated with the data), and
 #'   time_series ( list containing a tibble with the values of the time series).
 #'
-#'   The samples are from many sources. It has 13 classes ("Cerrado",
+#'   The samples are from many sources. It has 9 classes ("Cerrado",
 #'   "Fallow_Cotton", "Forest", "Millet_Cotton", "Pasture",
 #'   "Soy_Corn", "Soy_Cotton", "Soy_Fallow", "Soy_Millet").
 #'
@@ -96,5 +97,33 @@ NULL
 #'
 #' @usage
 #' data("br_mt_2K_9classes_6bands")
+#'
+NULL
+
+
+#' @title Samples of Cerrado biome
+#' @name br_mt_1_8K_9classes_6bands
+#' @docType data
+#'
+#' @description A dataset containing a sits tibble with time series samples from Brazilian
+#' Mato Grosso State (Amazon and Cerrado biomes).
+#'
+#' @format A \code{sits} tibble with 1.8K samples. A \code{sits} tibble contains the following meta-data:
+#'   longitude (East-west coordinate of the time series sample in WGS 84),
+#'   latitude (North-south coordinate of the time series sample in WGS 84),
+#'   start_date (initial date of the time series),
+#'   end_date (final date of the time series),
+#'   label (the class label associated to the sample),
+#'   coverage (the name of the coverage associated with the data), and
+#'   time_series ( list containing a tibble with the values of the time series).
+#'
+#'   The samples are from many sources. It has 9 classes ("Cerrado",
+#'   "Fallow_Cotton", "Forest", "Millet_Cotton", "Pasture",
+#'   "Soy_Corn", "Soy_Cotton", "Soy_Fallow", "Soy_Millet").
+#'
+#'   Each time series comprehends 12 months (23 data points) from MOD13Q1 product, and has 6 bands.
+#'
+#' @usage
+#' data("br_mt_1_8K_9classes_6bands")
 #'
 NULL
--- R/qgis_style.R
+++ R/qgis_style.R
@@ -123,6 +123,37 @@ qgis_style <- function(dataset, qml_file = "") {
   </pipe>
   <blendMode>0</blendMode>
 </qgis>", file = qml_file)
+         },
+         "br_mt_1_8K_9classes_6bands" = {
+           cat("
+<!DOCTYPE qgis PUBLIC 'http://mrcc.com/qgis.dtd' 'SYSTEM'>
+  <qgis version=\"2.18.9\" minimumScale=\"inf\" maximumScale=\"1e+08\" hasScaleBasedVisibilityFlag=\"0\">
+    <pipe>
+      <rasterrenderer opacity=\"1\" alphaBand=\"0\" band=\"1\" type=\"paletted\">
+        <rasterTransparency>
+          <singleValuePixelList>
+            <pixelListEntry min=\"0\" max=\"0\" percentTransparent=\"100\"/>
+          </singleValuePixelList>
+        </rasterTransparency>
+        <colorPalette>
+          <paletteEntry value=\"0\" color=\"#000000\" label=\"0\"/>
+          <paletteEntry value=\"1\" color=\"#35811D\" label=\"1. Cerrado\"/>
+          <paletteEntry value=\"2\" color=\"#d4d6ed\" label=\"2. Fallow_Cotton\"/>
+          <paletteEntry value=\"3\" color=\"#35811D\" label=\"3. Forest\"/>
+          <paletteEntry value=\"4\" color=\"#FFDFAA\" label=\"4. Pasture\"/>
+          <paletteEntry value=\"5\" color=\"#813E11\" label=\"5. Soy_Corn\"/>
+          <paletteEntry value=\"6\" color=\"#811911\" label=\"6. Soy_Cotton\"/>
+          <paletteEntry value=\"7\" color=\"#AA6639\" label=\"7. Soy_Fallow\"/>
+          <paletteEntry value=\"8\" color=\"#AA4139\" label=\"8. Soy_Millet\"/>
+          <paletteEntry value=\"9\" color=\"#AA4139\" label=\"9. Soy_Sunflower\"/>
+        </colorPalette>
+      </rasterrenderer>
+      <brightnesscontrast brightness=\"0\" contrast=\"0\"/>
+      <huesaturation colorizeGreen=\"128\" colorizeOn=\"0\" colorizeRed=\"255\" colorizeBlue=\"128\" grayscaleMode=\"0\" saturation=\"0\" colorizeStrength=\"100\"/>
+      <rasterresampler maxOversampling=\"2\"/>
+    </pipe>
+    <blendMode>0</blendMode>
+  </qgis>", file = qml_file)
          })
 
   if (qml_file != "")
